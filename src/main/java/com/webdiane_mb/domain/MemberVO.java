81ea22607c728e39cdb41fa43d79052928cce5b8
DianeChoi14 <miseol914@gmail.com>
2024-07-08 14:13:52 +0900

    feat : MemberVO생성

---
+++ src/main/java/com/webdiane_mb/domain/MemberVO.java
@@ -0,0 +1,30 @@
+package com.webdiane_mb.domain;
+
+import java.sql.Timestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@ToString
+@Builder
+public class MemberVO
+{
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String mobile;
+	private String email;
+	private Timestamp registerDate;
+	private String userImg;
+	private int userPoint;
+	
+}
