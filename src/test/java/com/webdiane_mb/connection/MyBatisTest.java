798bf8599c879225062ed2162b5d011b5a808634
DianeChoi14 <miseol914@gmail.com>
2024-07-08 11:58:05 +0900

    mybatis sqlSessiong 생성

--- src/test/java/com/webdiane_mb/connection/MyBatisTest.java
+++ src/test/java/com/webdiane_mb/connection/MyBatisTest.java
@@ -1,11 +1,9 @@
 package com.webdiane_mb.connection;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import javax.inject.Inject;
-import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,20 +14,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 (
 	locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"} // 하드디스크(file), **로드할 파일 사이에 폴더가 위치할 수 있음을 암시
 )
-public class DataSourceTest 
-{	
+public class MyBatisTest 
+{
 	@Inject
-	private DataSource ds; // Spring container(스프링메모리)에서 데이터타입이 DataSource인 객체를 찾아-설정파일(root-context.xml)을-의존성(객체)주입
-	// 스프링에서는 new하여 객체를 생성할 필요 없이 스프링에서 관리하고 있는 데이터를 사용
-	
+	private SqlSessionFactory sqlFactory; // Spring 컨테이너에서 SqlSessionFactory 객체를 찾아 주입
 	
 	@Test
-	public void testConnection() throws SQLException
+	public void testSqlSessionFactory()
 	{
-		//System.out.println("ds출력"+ds.toString());
+		System.out.println("연결확인 :" + sqlFactory.toString());
 		
-		Connection con = ds.getConnection();
-		if(con!=null) System.out.println("연결성공");
+		SqlSession session = sqlFactory.openSession();
+		if(session!=null) System.out.println("세션확인 :" + session.toString());
 	}
-	
 }
